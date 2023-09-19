31aef9babbc7a262edb97ffe5b2819135abafb38
maigiabao123 <maigiabao22082010@gmail.com>
2023-09-19 20:32:07 +0700

    new commit

---
+++ DAY2/day2.js
@@ -0,0 +1,50 @@
+// // let person = {
+// //     name : "Phương",
+// //     age: 40,
+// //     job: "công việc tự do",
+// //     address : "cầu giấy, hà nội",
+// //     favor: ["làm việc", "xem điện thoại" , "mua sắm"]
+// // };
+// // console.log(person.name);
+// // console.log(person.age);
+// // console.log(person.job);
+// // console.log(person.address);
+// // console.log(person.favor);
+
+// // function sayhello() {
+// //     console.log("Hello Anh");
+// // }
+// // sayhello();
+
+// function countdivide(x, y) {
+//     let z = x / y
+//     console.log(`${x} / ${y} =${z}`);
+// }
+// countdivide(10, 5)
+
+// function countmultiple(x, y) {
+//     let z = x * y
+//     console.log(`${x} * ${y} =${z}`);
+// }
+// countmultiple(10, 5)
+
+// function countminus(x, y) {
+//     let z = x - y
+//     console.log(`${x} - ${y} =${z}`);
+// }
+// countminus(10, 5)
+
+
+// function myFullname(name) {
+//     return name;
+// }
+
+// console.log(myFullname("mai gia bảo"));
+
+for(let i = 0; i <= 100;i++) {
+    if(i % 3 ==0 && (i -2) % 10 ==0 && i % 12 ==0){
+        console.log(i);
+    }
+   
+}
+
